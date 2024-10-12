00948c3a3223aaa9f45ff112c1ed92e6876e0718
sorza <alexandre.sorza@outlook.com>
2024-10-12 19:09:19 -0300

    Definindo Entidades

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,12 @@
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
