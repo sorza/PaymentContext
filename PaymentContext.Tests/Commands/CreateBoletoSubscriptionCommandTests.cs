d446d816787e3862dd6ff579f15794259d10457e
sorza <alexandre.sorza@outlook.com>
2024-10-13 18:53:38 -0300

    Testando CreateBoletoSubscriptionCommand

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,20 @@
+
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNomeIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.IsValid);
+        }
+        
+    }
+}
